7fe6f995aba44870db0753b198c23a74aaedfd84
Nathaniel Bender <nsb2@students.calvin.edu>
2016-03-23 20:32:54 -0400

    Added directory selection menu

--- mp3organize.pl
+++ mp3organize.pl
@@ -1,9 +1,20 @@
 #!/usr/bin/perl
 
 use Tk;
+use Encode;
+
 my $mw = new MainWindow; #Creates an instance of the window that will contain the widgets
 my $label = $mw -> Label(-text=>"Hello World") -> pack();
-my $button = $mw -> Button(-text => "Quit", 
+
+my $exitButton = $mw -> Button(-text => "Quit", 
         -command => sub { exit })
     -> pack();
+
+my $chooseDirLabel = $mw -> Label(-text=>"Please choose the directory containing the files:") -> pack();
+my $directoryButton = $mw -> Button(-text => "Select Directory",
+        -command => sub { my $dir = $mw->chooseDirectory;
+		$die = encode("windows-1252", $dir) })
+    -> pack();
+
+
 MainLoop;
